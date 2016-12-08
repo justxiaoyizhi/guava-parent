9d425fc56db801a074bd11e2b7d9397da02ba32d
justxiaoyizhi <18702528592@163.com>
2016-12-09 00:44:50 +0800

    Initial commit

---
+++ guava-custom/test/main/java/com/xyz/basicutils/MapNull.java
@@ -0,0 +1,26 @@
+package main.java.com.xyz.basicutils;
+
+
+import junit.framework.Assert;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Xiao on 2016/12/8.
+ */
+@Slf4j
+public class MapNull {
+
+    @Test
+    public void mapValueNull() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","null");
+        Assert.assertEquals("null",map.get("name"));
+        Assert.assertNull(map.get("123"));
+        log.debug("这个key为name的值是:{}",map.get("name"));
+        log.debug("这个key为123的值是{}",map.get("123"));
+    }
+}
